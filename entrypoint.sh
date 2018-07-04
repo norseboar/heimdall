eb3f5c94e908d139768b2b555e2e3cb70635a79d
joshma <me@joshma.com>
2018-07-04 09:35:50 -0700

    Fix configuration to run in development (#2)
    - If SLACK_TOKEN is set, do not override it - while this isn't actually
      used in practice, seems nice to keep container image "portable"
    - Use docker-compose secrets to manage the slack token in development -
      reads slack_token.txt to configure - it creates a file called
      /run/secrets/slack_token on the container
    - Because docker-compose is the recommended way now, remove the
      app.run() call in app.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,14 @@
 set -euo pipefail
 
 BIND="$1"
-# Read SLACK_TOKEN to configure app
-export SLACK_TOKEN="$(aws ssm get-parameters --names /heimdall/slack-token --with-decryption --output text | cut -f4)"
+if [[ -z "${SLACK_TOKEN+x}" ]]; then
+  # Read SLACK_TOKEN to configure app if not set:
+  # /run/secrets for dev mode (docker-compose)
+  # aws ssm for prod mode (AWS ECS)
+  export SLACK_TOKEN="$(
+    cat /run/secrets/slack_token \
+    || aws ssm get-parameters --names /heimdall/slack-token --with-decryption --output text | cut -f4
+  )"
+fi
 
 gunicorn --bind "$BIND" --access-logfile - wsgi
